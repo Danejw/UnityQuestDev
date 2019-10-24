7864d4935b665134c8791ac38889841cec4c6fa6
Danejw <djw.learn@gmail.com>
2019-10-24 15:27:13 -0700

    Installed Useful Tools
    Imported Probuilder
    Imported ProGrids
    Imported OpenVR (Desktop)
    
    Created an Interactable Enviroment

---
+++ QuestDevTemplate/Assets/Scripts/LeftHandHapticPulse.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class LeftHandHapticPulse : MonoBehaviour
+{
+    OVRHapticsClip pulse;
+    [SerializeField] private AudioClip audioClip;
+
+    private void Start()
+    {
+        pulse = new OVRHapticsClip(audioClip);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Grabbable")
+        {
+            OVRHaptics.LeftChannel.Mix(pulse);
+        }
+    }
+}
---
+++ QuestDevTemplate/Assets/Scripts/RightHandHapticPulse.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class RightHandHapticPulse : MonoBehaviour
+{
+    OVRHapticsClip pulse;
+    [SerializeField] private AudioClip audioClip;
+
+    private void Start()
+    {
+        pulse = new OVRHapticsClip(audioClip);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Grabbable")
+        {
+            OVRHaptics.RightChannel.Mix(pulse);
+        }
+    }
+}
